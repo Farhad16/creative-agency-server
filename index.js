b046d1e98784ac0cd9321dc6900c6864aeedbb24
Farhad16 <fhossain389@gmail.com>
2020-10-16 01:00:06 +0600

    All api is created and it works

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const fileUpload = require('express-fileupload');
 const fs = require('fs-extra');
+const ObjectId = require("mongodb").ObjectID;
 const port = 5000;
 require('dotenv').config();
 
@@ -27,6 +28,7 @@ client.connect(err => {
     const servicesCollection = client.db("creativeAgency").collection("services");
     const adminCollection = client.db("creativeAgency").collection("admin");
     const feedbacksCollection = client.db("creativeAgency").collection("feedbacks");
+    const ordersCollection = client.db("creativeAgency").collection("orders");
 
     app.post('/addService', (req, res) => {
         const file = req.files.file;
@@ -90,8 +92,90 @@ client.connect(err => {
             })
     })
 
+    app.post('/addOrder', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const email = req.body.email;
+        const orderName = req.body.orderName;
+        const projectDetails = req.body.projectDetails;
+        const price = req.body.price;
+        const status = req.body.status;
+        const filePath = `${__dirname}/orders/${file.name}`;
 
+
+        file.mv(filePath, err => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send({ msg: "Failed to upload Image" })
+            }
+            res.send({ name: file.name, path: `/${file.name}` });
+
+            const newImg = fs.readFileSync(filePath);
+            const encImg = newImg.toString('base64');
+
+            const image = {
+                contentType: file.mimetype,
+                size: file.size,
+                img: Buffer.from(encImg, 'base64')
+            };
+
+            ordersCollection.insertOne({ name, email, orderName, projectDetails, price, image, status })
+                .then(result => {
+                    res.send(result.insertedCount > 0)
+                });
+        });
+
+    });
+
+    app.get('/orders/:email', (req, res) => {
+        const email = req.params.email
+        adminCollection.find({ email: email })
+            .toArray((err, admin) => {
+                if (admin.length > 0) {
+                    ordersCollection.find({})
+                        .toArray((err, documents) => {
+                            res.send(documents);
+                        })
+                }
+                else {
+                    ordersCollection.find({ email: email })
+                        .toArray((err, documents) => {
+                            res.send(documents);
+                        })
+                }
+
+            })
+    });
+
+
+
+    app.patch("/updateStatus/:id", (req, res) => {
+        ordersCollection.updateOne(
+            {
+                _id: ObjectId(req.params.id),
+            },
+            {
+                $set: {
+                    status: req.body.status,
+                },
+            }
+        )
+            .then((result) => {
+                res.send(result.modifiedCount > 0);
+            });
+    });
+
+
+    app.post('/makeAdmin', (req, res) => {
+        const email = req.body.email;
+
+        adminCollection.insertOne({ email: email })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            });
+    })
 });
+
 
 
 
